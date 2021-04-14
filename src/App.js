29d9b27dbbfd48da0924b08b98951b38b4c8e8f1
Your Name <you@example.com>
2021-04-14 15:46:58 +0600

    added background image

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { Route, Switch } from 'react-router';
 import { NavLink } from 'react-router-dom';
 import './App.css';
 import About from './components/About/About';
+import LogIn from './components/LogIn/LogIn';
+import Services from './components/Services/Services';
+import SignUp from './components/SignUp/SignUp';
 import Contact from './components/Contact/Contact';
 import Home from './components/Home/Home';
 // import NotFound from './components/NotFound/NotFound';
@@ -20,6 +23,9 @@ function App() {
         <li><NavLink activeClassName="activeLink" activeStyle = {activeLinkStyle} to="/About">About</NavLink></li>
         <li><NavLink activeClassName="activeLink" activeStyle = {activeLinkStyle} to="/posts">Posts</NavLink></li>
         <li><NavLink activeClassName="activeLink" activeStyle = {activeLinkStyle} to="/contact">Contact</NavLink></li>
+        <li><NavLink activeClassName="activeLink" activeStyle = {activeLinkStyle} to="/login">Contact</NavLink></li>
+        <li><NavLink activeClassName="activeLink" activeStyle = {activeLinkStyle} to="/signup">Contact</NavLink></li>
+        <li><NavLink activeClassName="activeLink" activeStyle = {activeLinkStyle} to="/services">Contact</NavLink></li>
       </ul>
       <Switch>
         <div className="components">
@@ -27,6 +33,9 @@ function App() {
         <Route path="/posts" component={Posts} />
         <Route path="/contact" component={Contact} />
         <Route exact path="/" component={Home} />
+        <Route path="/login" component={LogIn} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/services" component={Services} />
         {/* <Route path="/" component={NotFound} /> */}
         </div>
       </Switch>
